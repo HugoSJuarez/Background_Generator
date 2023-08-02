4f8f962f20cab29b3b420fe52b378c4db4069c6a
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-01 21:26:06 -0600

    added change of background with only two colors

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+const body = document.querySelector("body");
 const inputColors = document.querySelectorAll(".color-input");
 const colorFront = document.querySelectorAll(".color-front");
+const selectGradient = document.querySelector("#type-gradient");
+let typeGradient = selectGradient.value;
+
+
+
+const listColors = ["#00ff00", "#ff0000"];
+addBackground();
+
+function addBackground(){
+    let styleBg = typeGradient + "(to right, ";
+    listColors.forEach((c,j) =>{
+        styleBg += c;
+        if( j !== (listColors.length-1)){
+            styleBg += ", ";
+        }
+    });
+    styleBg += ")";
+    body.style.backgroundImage = styleBg;
+    console.log(styleBg)
+    console.log(body.style.backgroundImage);
+}
 
 inputColors.forEach((input,i) => {
-    input.addEventListener("change",(e)=>{
+    input.addEventListener("input",(e)=>{
         let color = e.target.value;
         colorFront[i].style.backgroundColor=color;
-    })
+        listColors[i]=color;
+        
+        addBackground();
+    });
 });
 
 colorFront.forEach((color, i) =>{
     color.addEventListener("click", ()=>{
         inputColors[i].click();
-    })
-})
+    });
+});
