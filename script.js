04fda827eb9e5f6edc42fda84cefa790634a0946
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-02 15:38:54 -0600

    added functionanility of percentage and radial-gradient

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const body = document.querySelector("body");
 const inputColors = document.querySelectorAll(".color-input");
 const colorFront = document.querySelectorAll(".color-front");
+const colorPercent = document.querySelectorAll(".color-percent-select");
+
 const selectGradient = document.querySelector("#type-gradient");
 
 const listColors = ["#4776E6", "#8E54E9"];
+const listPercentColors = [0, 100];
 let typeGradient = selectGradient.value;
 
 addGradientBackground();
@@ -25,18 +28,31 @@ inputColors.forEach((input,i) => {
 
 selectGradient.addEventListener("change",()=>{
     typeGradient=selectGradient.value;
-    console.log(typeGradient);
     addGradientBackground();
-})
+});
+
+colorPercent.forEach((percent,k)=>{
+    percent.addEventListener("input",(e)=>{
+        let percentValue = ( Math.floor( Number(e.target.value)) || 0 );
+        percentValue = (percentValue>100) ? 100 : (percentValue <0) ? 0 : percentValue;
+        e.target.value = percentValue;
+        listPercentColors[k]=percentValue;
+        addGradientBackground();
+    });
+});
 
 function addGradientBackground(){
-    let styleBg = typeGradient + "(90deg , ";
+    let styleBg = typeGradient;
+    styleBg += (typeGradient!=="radial-gradient") ? "(90deg ," : "(";
     listColors.forEach((color,j) =>{
-        styleBg += color;
+        styleBg += color + " "+ listPercentColors[j] + "%";
         if( j !== (listColors.length-1)){
             styleBg += ", ";
         }
     });
     styleBg += ")";
+    console.log(styleBg);
     body.style.backgroundImage = styleBg;
 }
+
+
