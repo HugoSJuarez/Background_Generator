17e57e8a9ed4ea60da396c69eee962f75ff5e03b
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-01 21:54:49 -0600

    added style to percentage

--- script.js
+++ script.js
@@ -2,39 +2,41 @@ const body = document.querySelector("body");
 const inputColors = document.querySelectorAll(".color-input");
 const colorFront = document.querySelectorAll(".color-front");
 const selectGradient = document.querySelector("#type-gradient");
-let typeGradient = selectGradient.value;
-
-
 
 const listColors = ["#00ff00", "#ff0000"];
-addBackground();
+let typeGradient = selectGradient.value;
 
-function addBackground(){
-    let styleBg = typeGradient + "(to right, ";
-    listColors.forEach((c,j) =>{
-        styleBg += c;
-        if( j !== (listColors.length-1)){
-            styleBg += ", ";
-        }
-    });
-    styleBg += ")";
-    body.style.backgroundImage = styleBg;
-    console.log(styleBg)
-    console.log(body.style.backgroundImage);
-}
-
-inputColors.forEach((input,i) => {
-    input.addEventListener("input",(e)=>{
-        let color = e.target.value;
-        colorFront[i].style.backgroundColor=color;
-        listColors[i]=color;
-        
-        addBackground();
-    });
-});
+addGradientBackground();
 
 colorFront.forEach((color, i) =>{
     color.addEventListener("click", ()=>{
         inputColors[i].click();
     });
 });
+
+inputColors.forEach((input,i) => {
+    input.addEventListener("input",(e)=>{
+        let color = e.target.value;
+        colorFront[i].style.backgroundColor=color;
+        listColors[i]=color;
+        addGradientBackground();
+    });
+});
+
+selectGradient.addEventListener("change",()=>{
+    typeGradient=selectGradient.value;
+    console.log(typeGradient);
+    addGradientBackground();
+})
+
+function addGradientBackground(){
+    let styleBg = typeGradient + "(90deg , ";
+    listColors.forEach((color,j) =>{
+        styleBg += color;
+        if( j !== (listColors.length-1)){
+            styleBg += ", ";
+        }
+    });
+    styleBg += ")";
+    body.style.backgroundImage = styleBg;
+}
