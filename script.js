751244c43d884f5f88f9a2ecd17e707818367c75
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-02 22:36:30 -0600

    added add color wheels functionality

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const colorFront = document.querySelectorAll(".color-front");
 const colorPercent = document.querySelectorAll(".color-percent-select");
 const selectGradient = document.querySelector("#type-gradient");
 const inputDegree = document.querySelector("#deg");
+const allColors = document.querySelector("#colors");
+const buttonAdd = document.querySelector("#add-color");
 
 const listColors = [];
 const listPercentColors = [];
 let typeGradient = selectGradient.value;
 let degrees = Number(inputDegree.value);
 
+const wheelLimit = 4;
+let numberWheels;
+
+// Gradient Generator
 
 inputColors.forEach((input,i) => {
     colorFront[i].style.backgroundColor = input.value;
@@ -23,7 +29,6 @@ inputColors.forEach((input,i) => {
 
 colorFront.forEach((color, i) =>{
     listColors.push(color.style.backgroundColor);
-    console.log(listColors);
     color.addEventListener("click", ()=>{
         inputColors[i].click();
     });
@@ -51,7 +56,17 @@ inputDegree.addEventListener("input",()=>{
     addGradientBackground();
 });
 
+// Add more colors
 
+buttonAdd.addEventListener("click", ()=>{
+    numberWheels=listColors.length + 1;
+    if (numberWheels <= wheelLimit) {
+        createColorWheel(numberWheels);
+        if(numberWheels === wheelLimit) {
+            buttonAdd.style.display = "none";
+        }
+    }
+});
 
 addGradientBackground();
 
@@ -69,4 +84,70 @@ function addGradientBackground(){
     body.style.backgroundImage = styleBg;
 }
 
+function createColorWheel(numberAdded){
+    // Create a color selection element
+    let colorSelection =  document.createElement("div");
+    colorSelection.classList.add("color-selection");
 
+    let deleteBtn = document.createElement("div");
+    deleteBtn.classList.add("btn-delete");
+
+    let btnLogo = document.createElement("i");
+    btnLogo.classList.add("fa-solid", "fa-trash");
+    deleteBtn.appendChild(btnLogo);
+    colorSelection.appendChild(deleteBtn);
+
+    let colorFront = document.createElement("span");
+    colorFront.setAttribute("id", ( "color-front"+ numberAdded ) );
+    colorFront.classList.add("color-front");
+    
+
+    let inputColor = document.createElement("input");
+    inputColor.classList.add("color-input");
+    inputColor.setAttribute("type","color");
+    inputColor.setAttribute("name",( "color" + numberAdded ));
+    inputColor.setAttribute("value", "#ffffff" );
+    colorFront.style.backgroundColor=inputColor.value;
+    
+    inputColor.addEventListener("input", (e)=>{
+        let color = e.target.value;
+        colorFront.style.backgroundColor = color;
+        listColors[numberAdded-1]=color;
+        addGradientBackground();
+    });
+    listColors.push(colorFront.style.backgroundColor);
+    colorFront.addEventListener("click", ()=>{
+        inputColor.click();
+    });
+    
+    colorSelection.appendChild(inputColor);
+    colorSelection.appendChild(colorFront);
+
+    let colorPercent = document.createElement("span");
+    colorPercent.classList.add("color-percent");
+
+    let inputPercent = document.createElement("input");
+    inputPercent.classList.add("color-percent-select");
+    inputPercent.setAttribute("type", "text");
+    inputPercent.setAttribute("value","100");
+    inputPercent.setAttribute("name", ( "color" + numberAdded + "-percent" ) );
+    colorPercent.appendChild(inputPercent);
+
+    let percentLogo = document.createElement("i");
+    percentLogo.classList.add("fa-solid", "fa-percent", "percentage");
+    colorPercent.appendChild(percentLogo);
+
+    listPercentColors.push(inputPercent.value);
+    inputPercent.addEventListener("input",(e)=>{
+        let percentValue = ( Math.floor( Number(e.target.value)) || 0 );
+        percentValue = (percentValue>100) ? 100 : (percentValue <0) ? 0 : percentValue;
+        e.target.value = percentValue;
+        listPercentColors[numberAdded-1]=percentValue.toString();
+        addGradientBackground();
+    });    
+
+    colorSelection.appendChild(colorPercent);
+
+    allColors.appendChild(colorSelection);
+    addGradientBackground();
+}
